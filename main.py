1e2337b42704b0f22d2457ec112b789532f6fbb4
SurenNazaryan <127138768+SurenNazaryan@users.noreply.github.com>
2025-01-20 05:31:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,63 +21,36 @@ def get_page_response_hh(keyword, page_number=0):
     response.raise_for_status()
     url = response.url
     response = response.json()
-    return {
-        'vacancies_count': response['found'],
-        'vacancies_info': response['items'],
-        'pages_count': response['pages']
-    }
+    return response['found'], response['items'], response['pages']
 
 
-def get_vacancies_hh(keyword):
-    pages_count = get_page_response_hh(keyword)['pages_count']
+def get_vacancies_hh(keyword, pages_count):
     page_number = 0
     vacancies = []
     while page_number < pages_count:
-        vacancies_info = (
-            get_page_response_hh(keyword, page_number)['vacancies_info']
-        )
-        vacancies.extend(vacancies_info)
+        _, vacancies_on_page, _ = get_page_response_hh(keyword, page_number)
+        vacancies.extend(vacancies_on_page)
         page_number += 1
     return vacancies
 
 
-def get_vacancies_salary_hh(keyword):
-    vacancies = get_vacancies_hh(keyword)
-    vacancies_salarys = []
+def get_vacancies_salary_hh(keyword, vacancies):
+    vacancies_salaries = []
     for vacancy in vacancies:
         if not vacancy['salary'] or vacancy['salary']['currency'] != 'RUR':
             continue
         min_salary = vacancy['salary']['from']
         max_salary = vacancy['salary']['to']
-        if min_salary is None:
+        if not min_salary:
             min_salary = 0
-        if max_salary is None:
+        if not max_salary:
             max_salary = 0
         average_salary = get_salary(min_salary, max_salary)
-        vacancies_salarys.append(average_salary)
-    return vacancies_salarys
+        vacancies_salaries.append(average_salary)
+    return vacancies_salaries
 
 
-def get_salaries_statistics_hh(languages):
-    salaries_statistics = {}
-    for language in languages:
-        keyword = f'Программист {language}'
-        vacancies_salarys = get_vacancies_salary_hh(keyword)
-        vacancies_found = get_page_response_hh(keyword)['vacancies_count']
-        vacancies_processed = len(vacancies_salarys)
-        if vacancies_processed == 0:
-            average_salary = 0
-        else:
-            average_salary = int(sum(vacancies_salarys) / vacancies_processed)
-        salaries_statistics[language] = {
-            "vacancies_found": vacancies_found,
-            "vacancies_processed": vacancies_processed,
-            "average_salary": average_salary
-        }
-    return salaries_statistics
-
-
-def get_page_response_sj(keyword, sj_api_token, page=0):
+def get_page_response_sj(keyword, sj_api_token, page_number=0):
     url = 'https://api.superjob.ru/2.0/vacancies'
     headers = {
         'X-Api-App-Id': sj_api_token
@@ -89,7 +62,7 @@ def get_page_response_sj(keyword, sj_api_token, page=0):
         'town': moscow_id,
         'count': vacancies_count_in_page,
         'period': last_month_period,
-        'page': page,
+        'page': page_number,
         'keyword': keyword
     }
     response = requests.get(url, headers=headers, params=params)
@@ -97,58 +70,29 @@ def get_page_response_sj(keyword, sj_api_token, page=0):
     response = response.json()
     vacancies_count = response['total']
     pages = math.ceil(vacancies_count / vacancies_count_in_page)
-    return {
-        'pages_count': pages,
-        'vacancies_count': vacancies_count,
-        'vacancies_info': response['objects']
-    }
+    return pages, vacancies_count, response['objects']
 
 
-def get_vacancies_sj(keyword, sj_api_token):
-    pages_count = get_page_response_sj(keyword, sj_api_token)['pages_count']
+def get_vacancies_sj(keyword, sj_api_token, pages_count):
     page = 0
     vacancies = []
     while page < pages_count:
-        vacancies_info = (
-            get_page_response_sj(keyword, sj_api_token, page)['vacancies_info']
-        )
-        vacancies.extend(vacancies_info)
+        *_, vacancies_on_page = get_page_response_sj(keyword, sj_api_token, page)
+        vacancies.extend(vacancies_on_page)
         page += 1
     return vacancies
 
 
-def get_vacancies_salary_sj(keyword, sj_api_token):
-    vacancies = get_vacancies_sj(keyword, sj_api_token)
-    vacancies_salarys = []
+def get_vacancies_salary_sj(keyword, sj_api_token, vacancies):
+    vacancies_salaries = []
     for vacancy in vacancies:
         min_salary = vacancy['payment_from']
         max_salary = vacancy['payment_to']
-        if vacancy['currency'] != 'rub' or min_salary == 0 and max_salary == 0:
+        if vacancy['currency'] != 'rub' or not min_salary and not max_salary:
             continue
         average_salary = get_salary(min_salary, max_salary)
-        vacancies_salarys.append(average_salary)
-    return vacancies_salarys
-
-
-def get_salaries_statistics_sj(languages, sj_api_token):
-    salaries_statistics = {}
-    for language in languages:
-        keyword = f'Программист {language}'
-        vacancies_salarys = get_vacancies_salary_sj(keyword, sj_api_token)
-        vacancies_found = (
-            get_page_response_sj(keyword, sj_api_token)['vacancies_count']
-        )
-        vacancies_processed = len(vacancies_salarys)
-        if vacancies_processed == 0:
-            average_salary = 0
-        else:
-            average_salary = int(sum(vacancies_salarys) / vacancies_processed)
-        salaries_statistics[language] = {
-            "vacancies_found": vacancies_found,
-            "vacancies_processed": vacancies_processed,
-            "average_salary": average_salary
-        }
-    return salaries_statistics
+        vacancies_salaries.append(average_salary)
+    return vacancies_salaries
 
 
 def get_statistics_table(salaries_statistics, title):
@@ -157,7 +101,7 @@ def get_statistics_table(salaries_statistics, title):
          'Средняя зарплата',
          'Вакансий найдено',
          'Вакансий обработано']
-    ]  # Заголовки столбцов
+    ]
     for language, stats in salaries_statistics.items():
         table_data.append(
             [language, 
@@ -171,13 +115,30 @@ def get_statistics_table(salaries_statistics, title):
 
 
 def get_salary(min_salary, max_salary):
-    if min_salary != 0 and max_salary == 0:
+    if min_salary and not max_salary:
         average_salary = int(min_salary * 1.2)
-    elif min_salary == 0 and max_salary != 0:
+    elif not min_salary and max_salary:
         average_salary = int(max_salary * 0.8)
     else:
         average_salary = int((min_salary + max_salary) / 2)
     return average_salary
+
+
+def calculate_statistics(language, get_page_response, get_vacancies, get_vacancies_salary, api_token=None):
+    keyword = f'Программист {language}'
+    if api_token:
+        pages_count, vacancies_count, _ = get_page_response(keyword, api_token)
+    else:
+        vacancies_count, _, pages_count = get_page_response(keyword)
+    vacancies = get_vacancies(keyword, api_token, pages_count)
+    vacancies_salaries = get_vacancies_salary(keyword, api_token, vacancies)
+    vacancies_processed = len(vacancies_salaries)
+    average_salary = int(sum(vacancies_salaries) / vacancies_processed) if vacancies_processed else 0
+    return {
+        "vacancies_found": vacancies_count,
+        "vacancies_processed": vacancies_processed,
+        "average_salary": average_salary
+    }
 
 
 if __name__ == '__main__':
@@ -195,10 +156,22 @@ if __name__ == '__main__':
         "Shell",
         "Go"
     ]
-    salaries_statistics_hh = get_salaries_statistics_hh(languages)
-    salaries_statistics_sj = get_salaries_statistics_sj(
-        languages,
-        sj_api_token
-    )
-    get_statistics_table(salaries_statistics_hh, 'HeadHunter Moscow')
-    get_statistics_table(salaries_statistics_sj, 'SuperJob Moscow')
+    hh_salaries_statistics = {}
+    sj_salaries_statistics = {}
+    for language in languages:
+        hh_salaries_statistics[language] = calculate_statistics(
+            language,
+            get_page_response_hh,
+            get_vacancies_hh,
+            get_vacancies_salary_hh
+        )
+    for language in languages:
+        sj_salaries_statistics[language] = calculate_statistics(
+            language,
+            get_page_response_sj,
+            get_vacancies_sj,
+            get_vacancies_salary_sj,
+            sj_api_token
+        )
+    get_statistics_table(hh_salaries_statistics, 'HeadHunter Moscow')
+    get_statistics_table(sj_salaries_statistics, 'SuperJob Moscow')
